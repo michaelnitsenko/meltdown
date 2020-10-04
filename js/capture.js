fd2da40548fd829bbadb3cf774beae2656a8b35f
Michael <>
2020-10-04 14:39:30 +0200

    Initial commit

---
+++ js/capture.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class SoundInput {
+    startCapturing(callback) {
+        navigator.mediaDevices.getUserMedia({
+            video: false, audio: true
+        }).then(stream => {
+            const AudioContext = window.AudioContext // Default
+                || window.webkitAudioContext; // Safari and old versions of Chrome
+            const context = new AudioContext();
+            const analyser = context.createAnalyser();
+            analyser.fftSize = 256;
+            console.log(analyser.maxDecibels, analyser.minDecibels)
+            const source = context.createMediaStreamSource(stream);
+            source.connect(analyser);
+            
+            callback(analyser);
+        }).catch(err => console.log(err));    
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// global
+const soundInput = new SoundInput();
+let canvas, c2d, analyser;
+
+function onLoad() {
+    canvas = document.createElement("canvas");
+    canvas.width = 800;
+    canvas.height = 256;
+    document.body.appendChild(canvas);
+
+    c2d = canvas.getContext("2d");
+
+    soundInput.startCapturing(function (soundInputAnalyser) {
+        analyser = soundInputAnalyser;
+        // TODO: loop
+        readFrequesncies();
+        //
+    });
+
+}
+
+// Audio
+
+function indexHue(index) {
+    return (Math.log2(index) % 1) * 360; // coloring by octave 
+}
+
+function readFrequesncies() {
+    const tds = new Uint8Array(analyser.fftSize);
+    const freqs = new Uint8Array(analyser.frequencyBinCount);
+
+    analyser.getByteFrequencyData(freqs);
+    analyser.getByteTimeDomainData(tds);
+    c2d.clearRect(0, 0, canvas.width, canvas.height);
+    c2d.save();
+    c2d.scale(canvas.width / analyser.fftSize, 1);
+
+    var s = 0;
+    for (let i = 0; i < analyser.frequencyBinCount; i++) {
+        const v = freqs[i];
+        c2d.fillStyle = `hsl(${indexHue(i)}, 50%, 50%)`;
+        c2d.fillRect(i * 2, canvas.height - v, 2, v);
+        s += v;
+    }
+
+    c2d.restore();
+}
