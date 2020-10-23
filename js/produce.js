bfaedf1bb3c45b9fa92d21a8fd641a154f0fbc62
Michael <>
2020-10-23 22:57:47 +0200

    Pulce production and local image uploading

--- js/produce.js
+++ js/produce.js
@@ -1,18 +1,32 @@
 "use strict";
 //https://marcgg.com/blog/2016/11/01/javascript-audio/#
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 class SoundOutput {
-    startProducingSound() {
+    constructor() {
+        this.context = null;
+        this.os = [];
+    }
+
+    startProducingSound(values, duration) {
         const AudioContext = window.AudioContext // Default
             || window.webkitAudioContext; // Safari and old versions of Chrome
-        const context = new AudioContext();
-        var o = context.createOscillator();
-        var g = context.createGain();
-
-        var frequency = 440.0
-        o.frequency.value = frequency
-        o.type = "sine";
-        o.connect(g);
-        o.connect(context.destination);
-        o.start();
+        this.context = new AudioContext();
+        this.os = [];
+        for (var i = 0; i < values.length; i += 100) {
+            var o = this.context.createOscillator();
+            var g = this.context.createGain();
+    
+            o.frequency.value = values[i];
+            o.type = "sine";
+            o.connect(g);
+            o.connect(this.context.destination);
+            o.start();
+            o.stop(this.context.currentTime + duration);
+            this.os.push(0);
+        }
     }
 }
