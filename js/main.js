7ef7409f5e93fa22d7a83215ab7a51b857cfa575
rdx <radikhs@gmail.com>
2020-10-23 23:50:13 +0300

    Make colors of bars go from blue to red.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const soundInput = new SoundInput();
 let canvas, c2d, analyser;
 
 const frequenciecReadIntervalMs = 80;
-const maxRGBColors = 2 ** 24
+
+const fftSize = 128;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -21,29 +22,19 @@ function onLoad() {
 
     soundInput.startCapturing(async function (soundInputAnalyser) {
         analyser = soundInputAnalyser;
-        // TODO: loop
         while (true) {
             await sleep(frequenciecReadIntervalMs);
             readFrequesncies();
         }
-        
-        //
     });
 
 }
 
 
 // Audio
-
-function indexHue(index) {
-    return (Math.log2(index) % 1) * 360; // coloring by octave 
-}
-
 function readFrequesncies() {
     const tds = new Uint8Array(analyser.fftSize);
     const freqs = new Uint8Array(analyser.frequencyBinCount);
-
-    const maxFreqNumber = analyser.fftSize / 2;
 
     analyser.getByteFrequencyData(freqs);
     analyser.getByteTimeDomainData(tds);
@@ -51,23 +42,11 @@ function readFrequesncies() {
     c2d.save();
     c2d.scale(canvas.width / analyser.fftSize, 1);
 
-    const signalPowerDelta = Math.abs(analyser.maxDecibels - analyser.minDecibels);
-    const colorsPerDecibell = maxRGBColors / maxFreqNumber;
-
-    // console.log("maxRGBColors", maxRGBColors)
-    // console.log("colorsPerDecibell", colorsPerDecibell)
-    // console.log("signalPowerDelta", signalPowerDelta)
-
     var s = 0;
     for (let i = 0; i < analyser.frequencyBinCount; i++) {
         const v = freqs[i];
 
-        const color = "#"+v.toString(16)+"0000";
-        if (i % 1000 == 0) {
-            console.log("v",v)
-            console.log("color",color);
-        }
-
+        const color = `hsl(${256 - v}, 50%, 50%)`;
         c2d.fillStyle = color;
         c2d.fillRect(i * 2, canvas.height - v, 2, v);
         s += v;
