3468d26e809bb583dd75ba72fc9028e066358b65
rdx <radikhs@gmail.com>
2020-10-23 22:53:02 +0300

    Add gradient flow color for bars

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 const soundInput = new SoundInput();
 let canvas, c2d, analyser;
 
-const frequenciecReadIntervalMs = 50;
+const frequenciecReadIntervalMs = 80;
+const maxRGBColors = 2 ** 24
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -42,16 +43,32 @@ function readFrequesncies() {
     const tds = new Uint8Array(analyser.fftSize);
     const freqs = new Uint8Array(analyser.frequencyBinCount);
 
+    const maxFreqNumber = analyser.fftSize / 2;
+
     analyser.getByteFrequencyData(freqs);
     analyser.getByteTimeDomainData(tds);
     c2d.clearRect(0, 0, canvas.width, canvas.height);
     c2d.save();
     c2d.scale(canvas.width / analyser.fftSize, 1);
 
+    const signalPowerDelta = Math.abs(analyser.maxDecibels - analyser.minDecibels);
+    const colorsPerDecibell = maxRGBColors / maxFreqNumber;
+
+    // console.log("maxRGBColors", maxRGBColors)
+    // console.log("colorsPerDecibell", colorsPerDecibell)
+    // console.log("signalPowerDelta", signalPowerDelta)
+
     var s = 0;
     for (let i = 0; i < analyser.frequencyBinCount; i++) {
         const v = freqs[i];
-        c2d.fillStyle = `#000`;
+
+        const color = "#"+v.toString(16)+"0000";
+        if (i % 1000 == 0) {
+            console.log("v",v)
+            console.log("color",color);
+        }
+
+        c2d.fillStyle = color;
         c2d.fillRect(i * 2, canvas.height - v, 2, v);
         s += v;
     }
