53e57f4f323efa6d1497ce26e88f7cacc41a2775
Michael <>
2020-10-24 19:57:43 +0200

    Streamer debug

--- js/streamer.js
+++ js/streamer.js
@@ -4,11 +4,17 @@
 const soundOutput = new SoundOutput();
 let uploader;
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function transmit() {
+    var img = document.getElementById("image");
+    let _canvas = document.createElement("canvas");
+    _canvas.width = img.width;
+    _canvas.height = img.height;
+    let _c2d = _canvas.getContext("2d");
+    _c2d.drawImage(img, 0, 0);
+    let imageData = _c2d.getImageData(0, 0, img.width, img.height);
+    processImageData(imageData);
 }
 
-// File
 
 function onFileIsLoaded(event){
     let imageData = new Uint8Array(event.target.result);
@@ -16,13 +22,7 @@ function onFileIsLoaded(event){
     var blobTempUrl = URL.createObjectURL(blob);
     var img = document.getElementById("image");
     img.onload = function() {
-        let _canvas = document.createElement("canvas");
-        _canvas.width = img.width;
-        _canvas.height = img.height;
-        let _c2d = _canvas.getContext("2d");
-        _c2d.drawImage(img, 0, 0);
-        let imageData = _c2d.getImageData(0, 0, img.width, img.height);
-        processImageData(imageData);
+        // TODO: UNHIDE BUTTON
     };
 
     img.src = blobTempUrl;
@@ -36,28 +36,25 @@ function onFileLoaderChange(event) {
 }
 
 async function processImageData(imageData) {
-
-    //[0, 1, 2, 3, 4, 5, 6, 7]
-
     let width = imageData.width;
     let height = imageData.height;
     let brightnessLines = [];
 
-    for (let i = 0; i < height; i += 1) {
+    for (let i = 0; i < height; i++) {
         let brightnessLine = [];
 
-        for (let j = 0; j < width; j += 1) {
+        for (let j = 0; j < width; j++) {
             let brightness = 0;
 
-            brightness += 0.299 * imageData.data[i * height + j * 4 + 0] / 255;
-            brightness += 0.587 * imageData.data[i * height + j * 4 + 1] / 255;
-            brightness += 0.114 * imageData.data[i * height + j * 4 + 2] / 255;
-                
+            brightness += 0.299 * imageData.data[i * width * 4  + j * 4 + 0] / 255;
+            brightness += 0.587 * imageData.data[i * width * 4 + j * 4 + 1] / 255;
+            brightness += 0.114 * imageData.data[i * width * 4 + j * 4 + 2] / 255;
+            
             brightnessLine.push(brightness);
         }
 
         brightnessLines.push(brightnessLine);
     }
 
-    soundOutput.broadcastImageLine(brightnessLines, 0.1);
+    soundOutput.broadcastImageLine(brightnessLines, 1);
 }
